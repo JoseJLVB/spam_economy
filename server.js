1e03d8c3694d7779a2bb3b1f07efde609227a8b1
Jose Luis Valcarcel Bravo <josevalcarcel@entrearaucarias.com>
2022-02-19 09:09:37 -0300

    modulo UUID ejecutado exitosamente guardando archivo con mensaje enviado

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const enviar_email = require('./email.js');
 const app = express();
 const axios = require('axios');
-const fs = require('fs');
 const uuid = require('uuid');
+const {v4 : uuidv4} = require('uuid')
+const fs = require('fs');
 
 
 app.use(express.static('static'))
@@ -12,20 +13,22 @@ app.get('/email', async (req, res) => {
 
     let emailContent = '';
 
-    const correos = req.query.correos
-    const asunto = req.query.asunto
-    const contenido = req.query.contenido
+    const { correos, asunto, contenido } = req.query;
 
     emailContent += contenido + '\n\n';
     const divisa = await (await axios.get(`https://mindicador.cl/api`)).data;
     const enviar = ['dolar', 'euro', 'uf', 'utm'];
+
     enviar.forEach((indicador) => {
     emailContent += `El valor del(a) ${divisa[indicador].nombre} para hoy es: ${divisa[indicador].valor} pesos chilenos. \n\n`;
     });
 
+    fs.writeFile(`correos/,${uuidv4()}.txt`,emailContent.replaceAll('<p>', '').replaceAll('</p>', ''),'utf-8',function () {});
+
+    
     console.log(emailContent);
 
-    //enviar_email(correos, asunto, contenido)
+    enviar_email(correos, asunto, emailContent)
 
     res.send('Email enviado')
 })
